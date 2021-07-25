d60a10fc0bad3adbb8ad909978a34a2776c188bf
Olivier Andriessen <olivier@andev.nl>
2021-07-25 20:21:53 +0000

    feat: 🎸 generate uptime metadata

--- services/armada-api/src/ping/index.js
+++ services/armada-api/src/ping/index.js
@@ -4,6 +4,120 @@ const createContext = require('../context/createContext')
 const assert = require('assert')
 const app = express()
 const moment = require('moment')
+const times = require('lodash/times')
+const constant = require('lodash/constant')
+
+const AMOUNT_TIME_DATA_SLOTS = 60 * 24
+
+const createTimeData = () => {
+    return times(AMOUNT_TIME_DATA_SLOTS, constant(0))
+}
+const parseTimeData = input => {
+    return input ? input.split('').map(i => ~~i) : createTimeData()
+}
+
+const stringifyTimeData = input => {
+    return input.join('')
+}
+
+const getCurrentSlotIndex = (date) => {
+    const today = moment.utc().startOf('day').valueOf()
+    const dateInMillis = moment.utc(date).valueOf()
+    const diff = dateInMillis - today
+    const slotIndex = ~~(diff / (60 * 1000))
+    return slotIndex
+}
+
+const applyUptimeToTimeData = (timeData, date) => {
+
+    const slotIndex = getCurrentSlotIndex(date)
+
+    console.log(date, slotIndex)
+    timeData = [
+        ...timeData
+    ]
+
+    timeData[slotIndex] = 1
+
+    return timeData
+}
+
+const getUptimePctForTimeData = (timeData, { firstMetric, isToday }) => {
+
+    if (isToday) {
+        const slotIndex = getCurrentSlotIndex(new Date())
+        timeData = timeData.slice(0, slotIndex + 1)
+    }
+
+    if (firstMetric) {
+        timeData = timeData.slice(timeData.indexOf(1), timeData.length)
+    }
+
+    // console.log('timeData', timeData.join(''))
+
+    return ~~((timeData.reduce((a, b) => a + b, 0) / timeData.length) * 100)
+}
+
+app.get('/ping/metrics', async (req, res) => {
+
+    let ctx = await createContext()
+
+    try {
+
+        const date = moment.utc().format('YYYY-MM-DD')
+
+        const startDate = moment.utc().subtract(90, 'days').format('YYYY-MM-DD')
+
+        const pools = await ctx.db.query('SELECT poolId as id, ticker FROM pools')
+
+
+        const poolsWithMetrics = await Promise.all(
+            pools.map(async pool => {
+
+                const metrics = await ctx.db.query('SELECT uptimePct, date FROM metrics WHERE date >= ? AND poolId = ? ORDER BY date DESC', [
+                    startDate,
+                    pool.id
+                ])
+
+                const [{ avgUptimePct }] = await ctx.db.query('SELECT AVG(uptimePct) as avgUptimePct FROM metrics WHERE poolId = ?', [
+                    pool.id
+                ])
+
+                const [{ totalCount: previousMetricCount }] = await ctx.db.query('SELECT COUNT(id) as `totalCount` FROM metrics WHERE date < ? AND poolId = ? LIMIT 1', [
+                    date,
+                    pool.id
+                ])
+
+                let [metric] = await ctx.db.query('SELECT * FROM metrics WHERE date = ? AND poolId = ? LIMIT 1', [
+                    date,
+                    pool.id
+                ])
+
+                return {
+                    ...pool,
+                    timeData: metric ? metric.data : null,
+                    firstMetric: metric ? !previousMetricCount : null,
+                    avgUptimePct,
+                    metrics: metrics.map(metric => ([metric.date, metric.uptimePct]))
+                }
+            })
+        )
+
+        res.send({
+            pools: poolsWithMetrics
+        })
+
+    } catch (e) {
+
+        console.log(e)
+        res.send('error')
+
+    } finally {
+        if (ctx) {
+            await ctx.destroy()
+        }
+    }
+})
 
 app.post(`/ping`, bodyParser.json(), async (req, res) => {
 
@@ -41,10 +155,21 @@ app.post(`/ping`, bodyParser.json(), async (req, res) => {
 
         const date = moment.utc().format('YYYY-MM-DD')
 
+        const [{ totalCount: previousMetricCount }] = await ctx.db.query('SELECT COUNT(id) as `totalCount` FROM metrics WHERE date < ? AND poolId = ? LIMIT 1', [
+            date,
+            pool.poolId
+        ])
+
         let [metric] = await ctx.db.query('SELECT * FROM metrics WHERE date = ? AND poolId = ? LIMIT 1', [
             date,
             pool.poolId
         ])
+
+        let timeData = parseTimeData(metric.data)
+
+        timeData = applyUptimeToTimeData(timeData, new Date())
+
+        // console.log(stringifyTimeData(timeData))
 
         if (!metric) {
 
@@ -52,6 +177,8 @@ app.post(`/ping`, bodyParser.json(), async (req, res) => {
                 date,
                 poolId: pool.poolId,
                 ticker: pool.ticker,
+                data: stringifyTimeData(timeData),
+                uptimePct: getUptimePctForTimeData(timeData, { firstMetric: !previousMetricCount, isToday: true }),
                 remainingKesPeriods,
                 slotHeight,
                 nodeVersion,
@@ -64,6 +191,8 @@ app.post(`/ping`, bodyParser.json(), async (req, res) => {
             await ctx.db.query('UPDATE metrics SET ? WHERE date = ? AND poolId = ?', [
                 {
                     ticker: pool.ticker,
+                    data: stringifyTimeData(timeData),
+                    uptimePct: getUptimePctForTimeData(timeData, { firstMetric: !previousMetricCount, isToday: true }),
                     remainingKesPeriods,
                     slotHeight,
                     nodeVersion,
